0460ecc809a72e27686fdcebf44074b735bce2d6
Ester Ventura <eventur2@terpmail.umd.edu>
2026-03-05 10:03:15 -0500

    Add about-css route

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask, render_template
-
 import os
-from flask import Flask, render_template
+
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-app = Flask(__name__, template_folder=os.path.join(BASE_DIR, "templates"))
-
+app = Flask(__name__)
 
 @app.route('/hello')
 def hello():
@@ -14,6 +12,10 @@ def hello():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/about-css')
+def about_css():
+    return render_template('about-css.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
